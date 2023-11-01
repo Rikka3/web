1f4604f5a0c55a5e1a082260e4e5509381be0599
Rikka3 <135252548+Rikka3@users.noreply.github.com>
2023-11-01 11:07:37 +0600

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const contactForm = document.querySelector('#contact form');
-
-    contactForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        alert('Message sent!');
-    });
-});
